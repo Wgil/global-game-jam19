9a67a8026ce0d0afb81a842d6d57918379fd2672
Douglas <bravolugo5@gmail.com>
2019-01-27 12:30:44 -0400

    Add menu

--- src/index.js
+++ src/index.js
@@ -1,10 +1,109 @@
 import 'phaser';
 import {COORDS, GAME_WIDTH, GAME_HEIGHT, PLAYER_SPEED, PARTICLES_QUANTITY, RESPAWN_DELAY} from './utils/constants'
-import {player, initPlayer} from './player'
-import {shapes, initShapes, NAMES as SHAPE_NAMES, respawnShapes} from './shapes'
+import Game from './game'
 
-// https://photonstorm.github.io/phaser3-docs/global.html#GameConfig
+let theme;
+
+class SceneA extends Phaser.Scene {
+
+    constructor ()
+    {
+        super({ key: 'sceneA' });
+    }
+
+    preload ()
+    {
+        // background
+        this.load.image('background', 'assets/background/space_bk.png');
+
+        // enemies
+        this.load.image('star_orange_01', 'assets/shapes/stars/star_orange01.png');
+        this.load.image('star_orange_02', 'assets/shapes/stars/star_orange02.png');
+        this.load.image('star_orange_03', 'assets/shapes/stars/star_orange03.png');
+
+        this.load.image('star_white_01', 'assets/shapes/stars/star_white01.png');
+        this.load.image('star_white_02', 'assets/shapes/stars/star_white02.png');
+        this.load.image('star_white_03', 'assets/shapes/stars/star_white03.png');
+
+        this.load.image('star_red_01', 'assets/shapes/stars/star_red01.png');
+        this.load.image('star_red_02', 'assets/shapes/stars/star_red02.png');
+        this.load.image('star_red_03', 'assets/shapes/stars/star_red03.png');
+
+        
+        // FX particles
+        this.load.image('spark', 'assets/fx/blue.png');
+
+        // player
+        this.load.image('player_01', 'assets/shapes/stars/star_blue01.png');
+        this.load.image('player_02', 'assets/shapes/stars/star_blue02.png');
+        this.load.image('player_03', 'assets/shapes/stars/star_blue03.png');
+
+        // MUSIC
+        this.load.audio('theme', [
+            'assets/music/Level_Theme_Carrera_de_luz.wav'
+        ]);
+
+        this.load.audio('theme_start', [
+            'assets/music/start_1846.wav'
+        ]);
+
+        this.load.audio('theme_start', [
+            'assets/music/start_1846.wav'
+        ]);
+
+        this.load.audio('menu_theme', [
+            'assets/music/menu_theme_la_luz.wav'
+        ]);
+        
+        // FX SOUNDS
+        this.load.audio('hit', [
+            'assets/sounds/hit.wav'
+        ]);
+
+        this.load.audio('point', [
+            'assets/sounds/point.wav'
+        ]);
+    }
+
+    create ()
+    {
+        theme = this.sound.add('menu_theme');
+        var loopMarker = {
+            name: 'loopx',
+            start: 0,
+            config: {
+                loop: true
+            }
+        };
+        theme.addMarker(loopMarker);
+        theme.play('loopx');
+
+
+        this.input.manager.enabled = true;
+
+        this.input.once('pointerdown', function () {
+            console.log("Clicked")
+            this.scene.start('Game');
+            theme.stop();
+
+        }, this);
+
+        this.cameras.main.fadeIn(7000);
+
+        // Instructions 
+        var instructions = this.add.text(90, COORDS.Y.center);
+        instructions.setText('Click on the screen to turn on the lights.')
+
+        var credits = this.add.text(190, GAME_HEIGHT - 200);
+        credits.setText('Arrow keys to play.');
+
+
+    }
+
+}
+
 var config = {
+    key: 'game',
     type: Phaser.AUTO,
     parent: 'phaser-example', //The DOM element that will contain the game canvas, or its id
     width: GAME_WIDTH, // The width of the game, in game pixels.
@@ -20,220 +119,7 @@ var config = {
         },
     },
     // Scenes fns
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+    scene: [SceneA, Game]
 };
 
-let game = new Phaser.Game(config);
-let cursors;
-let background;
-let particles;
-// new
-let text;
-let timedEvent;
-let theme;
-let theme_start;
-let hitSound;
-let pointSound;
-let gameOver;
-let hintText;
-
-
-function preload ()
-{
-    // background
-    this.load.image('background', 'assets/background/space_bk.png');
-
-    // enemies
-    this.load.image('star_orange_01', 'assets/shapes/stars/star_orange01.png');
-    this.load.image('star_orange_02', 'assets/shapes/stars/star_orange02.png');
-    this.load.image('star_orange_03', 'assets/shapes/stars/star_orange03.png');
-
-    this.load.image('star_white_01', 'assets/shapes/stars/star_white01.png');
-    this.load.image('star_white_02', 'assets/shapes/stars/star_white02.png');
-    this.load.image('star_white_03', 'assets/shapes/stars/star_white03.png');
-
-    this.load.image('star_red_01', 'assets/shapes/stars/star_red01.png');
-    this.load.image('star_red_02', 'assets/shapes/stars/star_red02.png');
-    this.load.image('star_red_03', 'assets/shapes/stars/star_red03.png');
-
-    
-    // FX particles
-    this.load.image('spark', 'assets/fx/blue.png');
-
-    // player
-    this.load.image('player_01', 'assets/shapes/stars/star_blue01.png');
-    this.load.image('player_02', 'assets/shapes/stars/star_blue02.png');
-    this.load.image('player_03', 'assets/shapes/stars/star_blue03.png');
-
-    // MUSIC
-    this.load.audio('theme', [
-        'assets/music/Level_Theme_Carrera_de_luz.wav'
-    ]);
-
-    this.load.audio('theme_start', [
-        'assets/music/start_1846.wav'
-    ]);
-    
-    // FX SOUNDS
-    this.load.audio('hit', [
-        'assets/sounds/hit.wav'
-    ]);
-
-    this.load.audio('point', [
-        'assets/sounds/point.wav'
-    ]);
-
-}
-
-
-
-function create () {   
-    // Create image Backgroud
-    background =
-        this.add.tileSprite(COORDS.X.center, COORDS.Y.center, GAME_WIDTH, GAME_HEIGHT, 'background');
-
-    // ADD MUSIC
-    theme = this.sound.add('theme');
-    var loopMarker = {
-        name: 'loop',
-        start: 0,
-        config: {
-            loop: true
-        }
-    };
-    theme.addMarker(loopMarker);
-
-    hitSound = this.sound.add('hit');
-    pointSound = this.sound.add('point');
-    theme_start = this.sound.add('theme_start');
-    theme_start.play();
-    this.cameras.main.fadeIn(7000);
-
-    // Init player options
-    initPlayer.apply(this);
-    
-    
-    // Init shape options
-    initShapes.apply(this);
-
-    // Callback to execute on player collide with shape
-    this.physics.add.overlap(player, shapes, collideShape, null, this);
-
-    //  Input Events
-    cursors = this.input.keyboard.createCursorKeys();
-
-    // Create text
-    text = this.add.text(32, 32);
-
-    // Create game over text
-    gameOver = this.add.text(170, COORDS.Y.center);
-
-    // Add hint text
-    hintText = this.add.text(200, COORDS.Y.center);
-    hintText.setText("Find your way home!");
-}
-
-function update() {
-    // update the position background
-    updateBackground()
-
-    if (!player.isAlive()) {
-        // Disable physics after collision
-        player.disableBody(true, false)
-
-        gameOver.setText("The end of the journey.");
-
-        // Show Game over
-        return
-    }
-
-    addPlayerMovement.apply(this);
-
-    text.setText(`Engaged: ${player.points}`);
-
-}
-
-function updateBackground () {
-    background.tilePositionY -= 1;
-}
-
-function collideShape(player, shape) {
-    if (!player.isHitten()) {
-
-
-        player.hitten = true;
-        shapes.generatePersonalities(shape);
-        initParticles.call(this)
-
-        theme.play('loop');
-        pointSound.play();
-        hintText.visible = false;
-
-        // Respawn shapes loop
-        this.time.addEvent({ delay: RESPAWN_DELAY, callback: respawnShapes, callbackScope: this, loop: true });
-
-    } else {
-        if (shape.evil) {
-            player.hitPlayer(player)
-            hitSound.play();
-        } else {
-            player.addPoints(player);
-            pointSound.play();
-        }
-
-        var quantity = PARTICLES_QUANTITY[2];
-        switch (player.hits) {
-            case 0:
-                quantity = PARTICLES_QUANTITY[2];
-                break;
-            case 1:
-                quantity = PARTICLES_QUANTITY[1];
-                break;
-            case 2:
-                quantity = PARTICLES_QUANTITY[0];
-                break;
-            default:
-                quantity = PARTICLES_QUANTITY[0];
-        }
-        
-        console.log("HITS:", player.hits)
-        console.log("POINTS", player.points)
-
-        setParticles(quantity)
-    }
-
-    // Disable physics after collision
-    shape.disableBody(true, true)
-}
-
-function addPlayerMovement() {
-    // add ease in movement or something
-    if (cursors.left.isDown) {
-        this.physics.moveTo(player, COORDS.X.left, player.y, PLAYER_SPEED)
-        
-    }
-    else if (cursors.right.isDown) {
-        this.physics.moveTo(player, COORDS.X.right, player.y, PLAYER_SPEED)
-    }
-
-    particles && particles.setPosition(player.x, player.y);
-}
-
-function initParticles() {
-    particles = this.add.particles('spark').createEmitter({
-        x: player.x,
-        y: player.y,
-        blendMode: 'SCREEN',
-        scale: { start: 0.2, end: 0 },
-        speed: { min: -100, max: 100 },
-        quantity: PARTICLES_QUANTITY[2]
-    });
-}
-
-function setParticles(quantitiy) {
-    particles.setQuantity(quantitiy);
-}
+var game = new Phaser.Game(config);
